642ad17e3037cc1ba6a20a536c322eb63d44330e
Xarmen <16585114+XarmenXepo@users.noreply.github.com>
2024-07-10 13:11:24 -0400

    Create week1_counter.c

---
+++ week1_counter.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+
+//while loop
+int counter = 3;
+while(counter > 0)
+{
+    printf("meow\n");
+    counter -= 1;
+}
+
+//for loop
+for (int counter2 = 0; counter < 3; counter++)
+printf("meow\n");
+
+
+//forever loop
+while(true)
+{
+    printf("meowwwww\n");
+}
