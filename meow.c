1fbde7e723ed067b6f7210330c53698407c453c9
Xarmen <16585114+XarmenXepo@users.noreply.github.com>
2024-07-10 13:05:05 -0400

    Create meow.c

---
+++ meow.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int meowing = 0;
+    while (meowing <= 5)
+    {
+        printf("meoooo00ooow\n");
+        meowing +=1;
+    }
+
+}
