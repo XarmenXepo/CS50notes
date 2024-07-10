a26f156c3a68191b7aa22c6e3e4b66546985fbe4
Xarmen <16585114+XarmenXepo@users.noreply.github.com>
2024-07-10 17:47:39 +0000

    Pending changes exported from your codespace

---
+++ week1_C/week1_boolean.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+if ( x < y )
+{
+    printf("X is greater than Y");
+}
+else
+{
+    printf("X is not greater than Y");
+}
+else
+{
+    printf("They are equal both X and Y");
+}
---
+++ week2_arrays/week2_buggy.c
@@ -0,0 +1,20 @@
+–//learning to use a debugger 'debug50' in C code
+#include <stdio.h>
+#include <cs50.h>
+
+void print_column (int height);
+
+int main (void)
+{
+    int h =get_int("Height = ");
+    print_column(h);
+}
+
+
+void print_column (int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        printf("#\n");
+    }
+}
